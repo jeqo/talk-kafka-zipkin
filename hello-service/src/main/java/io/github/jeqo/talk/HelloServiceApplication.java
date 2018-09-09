89c6de93ad62e1e6198c68cdaf21fd0d298152ae
Jorge Quilcate Otoya <quilcate.jorge@gmail.com>
2018-09-09 13:58:30 +0200

    feat: add tracing jaxrs and http-client

--- hello-service/src/main/java/io/github/jeqo/talk/HelloServiceApplication.java
+++ hello-service/src/main/java/io/github/jeqo/talk/HelloServiceApplication.java
@@ -1,22 +1,48 @@
 package io.github.jeqo.talk;
 
+import brave.Tracing;
+import brave.http.HttpTracing;
+import brave.httpclient.TracingHttpClientBuilder;
+import brave.jersey.server.TracingApplicationEventListener;
+import brave.sampler.Sampler;
 import io.dropwizard.Application;
-import io.dropwizard.client.HttpClientBuilder;
 import io.dropwizard.setup.Environment;
 import org.apache.http.client.HttpClient;
+import org.glassfish.jersey.server.monitoring.ApplicationEventListener;
+import zipkin2.Span;
+import zipkin2.reporter.AsyncReporter;
+import zipkin2.reporter.kafka11.KafkaSender;
 
 public class HelloServiceApplication extends Application<HelloServiceConfiguration> {
   @Override
   public void run(HelloServiceConfiguration configuration, Environment environment) {
-    final HttpClient httpClient =
-        new HttpClientBuilder(environment).using(configuration.getHttpClientConfiguration())
-            .build(getName());
+
+    /* START TRACING INSTRUMENTATION */
+    final KafkaSender sender = KafkaSender.newBuilder().bootstrapServers("localhost:29092").build();
+    final AsyncReporter<Span> reporter = AsyncReporter.builder(sender).build();
+    final Tracing tracing =
+        Tracing.newBuilder()
+            .localServiceName("hello-service")
+            .sampler(Sampler.ALWAYS_SAMPLE)
+            .spanReporter(reporter)
+            .build();
+    final HttpTracing httpTracing = HttpTracing.newBuilder(tracing).build();
+    final ApplicationEventListener jerseyTracingFilter = TracingApplicationEventListener.create(httpTracing);
+    environment.jersey().register(jerseyTracingFilter);
+    /* END TRACING INSTRUMENTATION */
+
+    // Without instrumentation
+//    final HttpClient httpClient =
+//        new HttpClientBuilder(environment).using(configuration.getHttpClientConfiguration())
+//            .build(getName());
+    final HttpClient httpClient = TracingHttpClientBuilder.create(httpTracing).build();
     final String url = configuration.getTranslationServiceUrl();
     final HelloTranslationServiceClient translationServiceClient =
         new HelloTranslationServiceClient(httpClient, url);
 
     final HelloResource helloResource = new HelloResource(translationServiceClient);
     environment.jersey().register(helloResource);
+
 
     final HelloServiceHealthCheck helloServiceHealthCheck = new HelloServiceHealthCheck();
     environment.healthChecks().register("hello-service", helloServiceHealthCheck);
