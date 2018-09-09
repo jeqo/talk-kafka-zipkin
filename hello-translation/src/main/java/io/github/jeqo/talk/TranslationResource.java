e58bc27a2e9896eb44d88662f8440c14fa27e6b0
Jorge Quilcate Otoya <quilcate.jorge@gmail.com>
2018-09-09 14:44:41 +0200

    feat: add custom span

--- hello-translation/src/main/java/io/github/jeqo/talk/TranslationResource.java
+++ hello-translation/src/main/java/io/github/jeqo/talk/TranslationResource.java
@@ -1,5 +1,9 @@
 package io.github.jeqo.talk;
 
+import brave.ScopedSpan;
+import brave.Tracer;
+import brave.Tracing;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -7,15 +11,29 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import static io.github.jeqo.talk.TranslationRepository.helloTranslations;
-
 @Path("translate")
 public class TranslationResource {
+
+  private final Tracer tracer;
+  private final TranslationRepository repository;
+
+  TranslationResource(TranslationRepository repository) {
+    this.repository = repository;
+    tracer = Tracing.currentTracer();
+  }
+
   @GET
   @Path("{lang}")
   @Produces(MediaType.TEXT_PLAIN)
   public Response translateHello(@PathParam("lang") final String lang) {
-    final String hello = helloTranslations.getOrDefault(lang, helloTranslations.get("en"));
+    /* START CUSTOM INSTRUMENTATION */
+    final ScopedSpan span = tracer.startScopedSpan("query-repository");
+    span.annotate("started-query");
+    span.tag("lang", lang);
+    final String hello = repository.find(lang);
+    span.annotate("finished-query");
+    span.finish();
+    /* END CUSTOM INSTRUMENTATION */
     return Response.ok(hello).build();
   }
 
--- hello-translation/src/main/java/io/github/jeqo/talk/TranslationServiceApplication.java
+++ hello-translation/src/main/java/io/github/jeqo/talk/TranslationServiceApplication.java
@@ -1,14 +1,38 @@
 package io.github.jeqo.talk;
 
+import brave.Tracing;
+import brave.http.HttpTracing;
+import brave.jersey.server.TracingApplicationEventListener;
+import brave.sampler.Sampler;
 import io.dropwizard.Application;
 import io.dropwizard.Configuration;
 import io.dropwizard.setup.Environment;
+import org.glassfish.jersey.server.monitoring.ApplicationEventListener;
+import zipkin2.Span;
+import zipkin2.reporter.AsyncReporter;
+import zipkin2.reporter.kafka11.KafkaSender;
 
 public class TranslationServiceApplication extends Application<Configuration> {
 
   @Override
   public void run(Configuration configuration, Environment environment) {
-    final TranslationResource translationResource = new TranslationResource();
+
+    /* START TRACING INSTRUMENTATION */
+    final KafkaSender sender = KafkaSender.newBuilder().bootstrapServers("localhost:29092").build();
+    final AsyncReporter<Span> reporter = AsyncReporter.builder(sender).build();
+    final Tracing tracing =
+        Tracing.newBuilder()
+            .localServiceName("translation-service")
+            .sampler(Sampler.ALWAYS_SAMPLE)
+            .spanReporter(reporter)
+            .build();
+    final HttpTracing httpTracing = HttpTracing.newBuilder(tracing).build();
+    final ApplicationEventListener jerseyTracingFilter = TracingApplicationEventListener.create(httpTracing);
+    environment.jersey().register(jerseyTracingFilter);
+    /* END TRACING INSTRUMENTATION */
+
+    final TranslationRepository repository = new TranslationRepository();
+    final TranslationResource translationResource = new TranslationResource(repository);
     environment.jersey().register(translationResource);
 
     final TranslationServiceHealthCheck healthCheck = new TranslationServiceHealthCheck();
